e5d55a659ba10a145adb1cfd3599038795b8f7ce
cxntii <145007988+cxntii@users.noreply.github.com>
2023-09-28 12:45:32 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+let zBereich;
+
+function setup() {
+  zBereich = createCanvas(windowWidth, windowHeight);
+  zBereich.position(0,0);
+  zBereich.style('z-Index','-1');
+
+}
+
+function draw() {
+  noStroke();
+  fill(255,255,255);
+  circle(mouseX,mouseY,24);
+  
+}
+  
+// function mousePressed() {
+//   background(151,255,255);
+// }
+  
